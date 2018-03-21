c56c333733775039f2a9526c6ff752769cb1cb06
GPaul <yarybakinfo@gmail.com>
2018-03-21 12:46:55 +0200

    Added auth_client, socials login

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -1,6 +1,24 @@
 <?php
 
-$config = [];
+$config = [
+    'components' => [
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'google' => [
+                    'class' => 'yii\authclient\clients\Google',
+                    'clientId' => 'google_client_id',
+                    'clientSecret' => 'google_client_secret',
+                ],
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => 'facebook_client_id',
+                    'clientSecret' => 'секретный_ключ_facebook_client',
+                ],
+            ],
+        ]
+    ],
+];
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
