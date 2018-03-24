d0fcdc18c43b245e0a904beaeef9f49ee3c1d148
GPaul <yarybakinfo@gmail.com>
2018-03-24 15:47:12 +0200

    Add compositeForm

--- shop/forms/manage/Shop/BrandForm.php
+++ shop/forms/manage/Shop/BrandForm.php
@@ -3,44 +3,29 @@
 namespace shop\forms\manage\Shop;
 
 use shop\entities\Shop\Brand;
+use shop\forms\CompositeForm;
 use shop\validators\SlugValidator;
 use shop\forms\manage\MetaForm;
-use yii\base\Model;
 
 /**
  * @property MetaForm $meta
  */
-class BrandForm extends Model
+class BrandForm extends CompositeForm
 {
     public $name;
     public $slug;
-
-    private $_meta;
     private $_brand;
-
-    public function __construct(Brand $brand = null, array $config = [])
+    public function __construct(Brand $brand = null, $config = [])
     {
-        if ($brand){
+        if ($brand) {
             $this->name = $brand->name;
             $this->slug = $brand->slug;
-            $this->_meta = new MetaForm($brand->meta);
+            $this->meta = new MetaForm($brand->meta);
             $this->_brand = $brand;
+        } else {
+            $this->meta = new MetaForm();
         }
         parent::__construct($config);
-    }
-
-    public function load($data, $formName = null): bool
-    {
-        $self = parent::load($data, $formName);
-        $meta = $this->_meta->load($data, $formName ? null : 'meta');
-        return $self && $meta;
-    }
-
-    public function validate($attributeNames = null, $clearErrors = true): bool
-    {
-        $self = parent::validate($attributeNames, $clearErrors);
-        $meta = $this->_meta->validate($attributeNames, $clearErrors);
-        return $self && $meta;
     }
 
     public function rules(): array
@@ -53,8 +38,8 @@ class BrandForm extends Model
         ];
     }
 
-    public function getMeta(): MetaForm
+    public function internalForms(): array
     {
-        return $this->_meta;
+        return ['meta'];
     }
 }
