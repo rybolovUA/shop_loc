7464998a1fb62f092cf92630f477c2fc63015372
GPaul <yarybakinfo@gmail.com>
2018-03-24 17:59:17 +0200

    Added catgory environment

---
+++ console/migrations/m180324_153837_create_shop_categories_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `shop_categories`.
+ */
+class m180324_153837_create_shop_categories_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        $this->createTable('{{%shop_categories}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull(),
+            'title' => $this->string(),
+            'description' => $this->text(),
+            'meta_json' => 'JSON NOT NULL',
+            'lft' => $this->integer()->notNull(),
+            'rgt' => $this->integer()->notNull(),
+            'depth' => $this->integer()->notNull(),
+        ], $tableOptions);
+        $this->createIndex('{{%idx-shop_categories-slug}}', '{{%shop_categories}}', 'slug', true);
+        $this->insert('{{%shop_categories}}', [
+            'id' => 1,
+            'name' => '',
+            'slug' => 'root',
+            'title' => null,
+            'description' => null,
+            'meta_json' => '{}',
+            'lft' => 1,
+            'rgt' => 2,
+            'depth' => 0,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%shop_categories}}');
+    }
+}
